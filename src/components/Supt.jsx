88d883a051603cdfa7ffd4603317d8a0fa20097d
NewtonHouseHostel <168670765+NewtonHouseHostel@users.noreply.github.com>
2025-10-07 22:59:28 +0530

    Update Supt.jsx

--- src/components/Supt.jsx
+++ src/components/Supt.jsx
@@ -1,5 +1,7 @@
-
-
+import cmvisit1 from '../assets/cmvisit1.jpg'
+import cmvisit2 from '../assets/cmvisit2.jpg'
+import cmvisit3 from '../assets/cmvisit3.jpg'
+import cmvisit4 from '../assets/cmvisit4.jpg'
 export default function Supt() {
   const link= "https://www.facebook.com/permalink.php?story_fbid=pfbid02BptrqUk541fkwehb8LKGqbi3meEywggqMo6ma9bKgFkGpz3oJu422A5Re9Arda3al&amp;id=100054738069406"
   return (
@@ -9,89 +11,24 @@ export default function Supt() {
           Latest from Superintendent <hr />{" "}
         </h1>
         <div className="supt-cornor w-4/5 m-auto grid grid-cols-2">
-          <div
-            className="fb-post"
-            data-href={link}
-            data-width="500"
-            data-show-text="true"
-          >
-            <blockquote
-              cite="https://www.facebook.com/permalink.php?story_fbid=945521000615827&amp;id=100054738069406"
-              class="fb-xfbml-parse-ignore"
-            >
-              <p>Here comes the sun☀️</p>Posted by{" "}
-              <a href="https://www.facebook.com/people/Nilesh-Yadav/100054738069406/">
-                Nilesh Yadav
-              </a>{" "}
-              on&nbsp;
-              <a href="https://www.facebook.com/permalink.php?story_fbid=945521000615827&amp;id=100054738069406">
-                Thursday 14 March 2024
-              </a>
-            </blockquote>
+         
+
+          
+
+          <div>
+            <img src={cmvisit1} alt="img" srcset="" width="" />
+          </div>
+          <div>
+            <img src={cmvisit2} alt="img" srcset="" width="" />
+          </div>
+          <div>
+            <img src={cmvisit3} alt="img" srcset="" width="" />
+          </div>
+          <div>
+            <img src={cmvisit4} alt="img" srcset="" width="" />
           </div>
 
-          <div
-            class="fb-post"
-            data-href="https://www.facebook.com/permalink.php?story_fbid=pfbid02BptrqUk541fkwehb8LKGqbi3meEywggqMo6ma9bKgFkGpz3oJu422A5Re9Arda3al&amp;id=100054738069406"
-            data-width="500"
-            data-show-text="true"
-          >
-            <blockquote
-              cite="https://www.facebook.com/permalink.php?story_fbid=945521000615827&amp;id=100054738069406"
-              class="fb-xfbml-parse-ignore"
-            >
-              <p>Here comes the sun☀️</p>Posted by{" "}
-              <a href="https://www.facebook.com/people/Nilesh-Yadav/100054738069406/">
-                Nilesh Yadav
-              </a>{" "}
-              on&nbsp;
-              <a href={"https://www.facebook.com/permalink.php?story_fbid=945521000615827&amp;id=100054738069406"}>
-                Thursday 14 March 2024
-              </a>
-            </blockquote>
-          </div>
-
-          <div
-            class="fb-post"
-            data-href="https://www.facebook.com/permalink.php?story_fbid=pfbid02BptrqUk541fkwehb8LKGqbi3meEywggqMo6ma9bKgFkGpz3oJu422A5Re9Arda3al&amp;id=100054738069406"
-            data-width="500"
-            data-show-text="true"
-          >
-            <blockquote
-              cite="https://www.facebook.com/permalink.php?story_fbid=945521000615827&amp;id=100054738069406"
-              class="fb-xfbml-parse-ignore"
-            >
-              <p>Here comes the sun☀️</p>Posted by{" "}
-              <a href="https://www.facebook.com/people/Nilesh-Yadav/100054738069406/">
-                Nilesh Yadav
-              </a>{" "}
-              on&nbsp;
-              <a href="https://www.facebook.com/permalink.php?story_fbid=945521000615827&amp;id=100054738069406">
-                Thursday 14 March 2024
-              </a>
-            </blockquote>
-          </div>
-
-          <div
-            class="fb-post"
-            data-href="https://www.facebook.com/permalink.php?story_fbid=pfbid02BptrqUk541fkwehb8LKGqbi3meEywggqMo6ma9bKgFkGpz3oJu422A5Re9Arda3al&amp;id=100054738069406"
-            data-width="500"
-            data-show-text="true"
-          >
-            <blockquote
-              cite="https://www.facebook.com/permalink.php?story_fbid=945521000615827&amp;id=100054738069406"
-              class="fb-xfbml-parse-ignore"
-            >
-              <p>Here comes the sun☀️</p>Posted by{" "}
-              <a href="https://www.facebook.com/people/Nilesh-Yadav/100054738069406/">
-                Nilesh Yadav
-              </a>{" "}
-              on&nbsp;
-              <a href="https://www.facebook.com/permalink.php?story_fbid=945521000615827&amp;id=100054738069406">
-                Thursday 14 March 2024
-              </a>
-            </blockquote>
-          </div>
+         
         </div>
       </div>
     </>
